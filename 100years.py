faf31584bb6134324f9a7045c68a9cfeaad6c331
TulioFusco <98853713+TulioFusco@users.noreply.github.com>
2022-02-02 23:25:05 -0300

    Add files via upload
    A mini-program that tells you in which year you will turn 100.

---
+++ 100years.py
@@ -0,0 +1,7 @@
+name = input('Please, enter your name: ')
+age = int(input('Please, enter your age: '))
+actual_year = 2022
+years_remaining = 100 - age
+year_hundred = years_remaining + actual_year
+print(f"Hello, {name}. You will be a hundred years old at {year_hundred}.")
+print(f"It will take {years_remaining} years.")
